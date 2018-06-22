47426545eb9793f0ba5529ef490c11d172e7df4a
lightning <aurora.hay@agile6.com>
2018-06-22 09:51:37 -0700

    add basic home page with readme

---
+++ config/initializers/markdown.rb
@@ -0,0 +1,10 @@
+MarkdownRails.configure do |config|
+  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+                                     :fenced_code_blocks => true,
+                                     :autolink => true,
+                                     :tables => true
+  )
+  config.render do |markdown_source|
+    markdown.render(markdown_source)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :yoga_poses, except: [:create, :update, :destroy]
   resources :yoga_categories, except: [:create, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'home#index'
 end
